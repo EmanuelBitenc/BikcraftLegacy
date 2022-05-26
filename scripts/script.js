24239306c38a7322d2568996c894ddff19c47e72
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2022-05-26 17:09:44 -0300

    Ativação de produto no orçamento

--- scripts/script.js
+++ scripts/script.js
@@ -8,3 +8,12 @@ function linkAtivo(link) {
     link.classList.add("ativo");
   }
 }
+
+//Checked da pagina orcamento
+const parametros = new URLSearchParams(location.search);
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+  console.log(elemento);
+}
+parametros.forEach(ativarProduto);
